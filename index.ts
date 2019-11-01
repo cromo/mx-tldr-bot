e8dc1ba3d71d100badb95ac6939d390a4b148804
Cristián Romo <cristian.g.romo@gmail.com>
2019-11-01 14:12:06 -0500

    Factor all logic into composed functions

--- index.ts
+++ index.ts
@@ -10,40 +10,34 @@ interface Config {
   smmryApiKey: string;
 };
 
-const config = getConfig("config.toml");
-const client = getClient(config.homeserverUrl, config.accessToken, new SimpleFsStorageProvider("sync.json"));
-
+const getClient = R.pipe(R.constructN(3, MatrixClient), R.tap(AutojoinRoomsMixin.setupOnClient));
+const readUtf8File = R.curryN(2, readFileSync)(R.__, {encoding: "utf-8"});
+const getConfig = R.pipe(readUtf8File, TOML.parse);
 const hasContent = R.compose(R.not, R.isNil, R.prop("content"));
 const eventBody = R.path(["content", "body"]);
 const startsWithBangCommand = command => R.pipe(eventBody, R.startsWith(`!${command}`));
 const shouldRespond = R.allPass([hasContent, startsWithBangCommand("tldr")]);
 const removeCommandPrefix = R.replace(/^!tldr\s*/, "");
 const commandContent = R.pipe(eventBody, removeCommandPrefix);
-const smmry = url => fetch(`https://api.smmry.com?SM_API_KEY=${config.smmryApiKey}&SM_URL=${url}`);
+const smmry = R.curry((apiKey, url) => fetch(`https://api.smmry.com?SM_API_KEY=${apiKey}&SM_URL=${url}`));
 const toJson = R.invoker(0, "json");
-const getSummary = R.pipe(smmry, R.then(toJson), R.then(R.prop("sm_api_content")));
+const getSummary = apiKey => R.pipe(smmry(apiKey), R.then(toJson), R.then(R.prop("sm_api_content")));
 const sendNotice = R.curry((client, roomId, message) => client.sendNotice(roomId, message));
+
+const config = getConfig("config.toml");
+const client = getClient(config.homeserverUrl, config.accessToken, new SimpleFsStorageProvider("sync.json"));
 
 client.start().then(() => console.log("Client started"));
 
 client.on('room.message', async (roomId: string, event: any) => {
+  const notifyRoom = sendNotice(client, roomId);
   R.when(
     shouldRespond,
     R.pipe(
       commandContent,
-      getSummary,
-      R.then(sendNotice(client, roomId)),
-      R.otherwise(R.thunkify(sendNotice)(client, roomId, `Unable to get summary for ${commandContent(event)}`))
+      getSummary(config.smmryApiKey),
+      R.then(notifyRoom),
+      R.otherwise(R.thunkify(notifyRoom)(`Unable to get summary for ${commandContent(event)}`))
     ),
     event);
 });
-
-function getConfig(filename: string) {
-  return TOML.parse(readFileSync(filename, {encoding: "utf-8"})) as unknown as Config;
-}
-
-function getClient(homeserverUrl: string, accessToken: string, storageProvider: IStorageProvider) {
-  const client = new MatrixClient(homeserverUrl, accessToken, storageProvider);
-  AutojoinRoomsMixin.setupOnClient(client);
-  return client;
-}
