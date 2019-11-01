62083721e63d03ab1b247229b00e29a9bd920712
Cristian Romo <cristian.g.romo@gmail.com>
2019-10-31 21:26:32 -0500

    Add reading config from TOML file

--- index.ts
+++ index.ts
@@ -1,7 +1,17 @@
+import { readFileSync } from "fs";
 import * as fetch from "node-fetch";
+import * as TOML from "@iarna/toml";
 
-const smmryApiKey = "";
+interface Config {
+  smmryApiKey: string;
+}
 
-fetch(`https://api.smmry.com?SM_API_KEY=${smmryApiKey}&SM_URL=https://buzzdecafe.github.io/code/2014/05/16/introducing-ramda`)
+const config = getConfig("config.toml");
+
+fetch(`https://api.smmry.com?SM_API_KEY=${config.smmryApiKey}&SM_URL=https://buzzdecafe.github.io/code/2014/05/16/introducing-ramda`)
     .then(res => res.json())
     .then(body => console.log(body));
+
+function getConfig(filename: string) {
+  return TOML.parse(readFileSync(filename, {encoding: "utf-8"})) as unknown as Config;
+}
